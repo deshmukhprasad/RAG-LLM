8f0105d2c5ea6fe7501ee28df1051fc0d42ec985
Prasad Deshmukh <deshmukhprasad@users.noreply.github.com>
2025-02-11 19:11:52 +0530

    Add files via upload

---
+++ train_movielens.sh
@@ -0,0 +1,20 @@
+python main.py \
+--mode train \
+--batch_size 1 \
+--accumulate_grad_batches 1 \
+--dataset movielens_data \
+--data_dir data/ref/movielens \
+--cans_num 20 \
+--prompt_path ./prompt/movie.txt \
+--rec_embed SASRec \
+--llm_tuning lora \
+--llm_path meta-llama/Llama-2-7b-hf \
+--rec_model_path ./rec_model/movielens.pt \
+--ckpt_dir ./checkpoints/movielens/ \
+--output_dir ./output/movielens/ \
+--log_dir movielens_logs \
+--lr_warmup_start_lr 8e-6 \
+--lr 8e-4 \
+--lr_decay_min_lr 8e-6 \
+--max_epochs 2 \
+--num_workers 0
